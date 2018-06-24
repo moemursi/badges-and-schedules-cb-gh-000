c36378e80590dca6a1afc26f9173530fe7cf150a
MO mursi <jemkarry@outlook.com>
2018-06-24 05:54:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+
+batch_badge_creator(names)
+  counter = 0
+  batch_message=[]
+  names.each do |name|
+     batch_message[counter]="Hello, my name is #{name}."
+     counter +=1
+  end  
+end
+names = ["ddd","ss","as"]
+batch_badge_creator(names)
